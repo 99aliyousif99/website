7c4a018a64e9ed11ac65b7b691b51d892b8398f9
99aliyousif99 <aljanabiali3@gmail.com>
2025-02-11 14:19:27 +0300

    add Container component and update App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
-
+import Container from "./Components/Container/Container";
 function App() {
   return (
     <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <Container>
+        <h1 className="">DeceptiConf</h1>
+        
+      </Container>
     </>
   );
 }
